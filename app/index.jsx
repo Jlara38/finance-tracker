205ad83d9ec47daf7b98e4c5f96ff7cf9dcb1185
Jlara38 <chuplaxlara@gmail.com>
2026-05-11 20:01:45 -0500

    More work on the homescreen has started.

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,7 @@
 import {StyleSheet, Text, View, Image, Button, Alert, Platform, ScrollView} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
+
+import SummaryCard from '../components/SummaryCard'
 import RecentExpenseCard from '../components/RecentExpenseCard'
 
 
@@ -8,22 +10,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.content}>
-        <Text style={styles.title}> This is the Home Page</Text>
-        <Image style={styles.img} source={Logo}/>
+    <SafeAreaView>
 
-        <Text style={{marginTop: 10, marginBottom: 30}}> Reading List App</Text>
+      <SummaryCard/>
 
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
-        
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
-        <RecentExpenseCard></RecentExpenseCard>
+      <ScrollView>
       </ScrollView>
     </SafeAreaView>
   )
@@ -34,6 +25,8 @@ export default Home
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
+    marginTop: 8
   },
   content:{
     alignItems: 'center',
---
+++ components/SummaryCard.jsx
@@ -0,0 +1,78 @@
+import react from 'react'
+import { StyleSheet, Text, View, colors, fontWeight} from 'react-native'
+import { verticalScale } from 'react-native-size-matters'
+
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export default function SummaryCard(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.totalBalanceRow}>
+                <Text>Total Balance</Text>
+            </View>
+
+            <Text style={styles.balance}> $2341.23 </Text>
+
+            <View style={styles.stats}>
+                {/* Income Column */}
+                <View style={{gap: verticalScale(5)}}>
+                    <View style={styles.incomeContainer}>
+                        <View style={styles.income}> 
+                            <AntDesign name="rise" size={24} color="black" />
+                            <Text>Income</Text>
+                        </View>
+
+                        <View style={{aligalignSelf: 'center'}}>
+                            <Text style={{color: 'green', size: 16, fontWeight: 'bold'}}> $500 </Text>
+                        </View>
+                    </View>
+                </View>
+
+                {/* Expense Column */}
+                <View style={{gap: verticalScale(5)}}>
+                    <View style={styles.incomeContainer}>
+                        <View style={styles.income}> 
+                            <AntDesign name="fall" size={24} color="black" />
+                            <Text>Expense</Text>
+                        </View>
+
+                        <View style={{aligalignSelf: 'center'}}>
+                            <Text style={{color: 'red', size: 16, fontWeight: 'bold'}}> $200 </Text>
+                        </View>
+                    </View>
+                </View>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignSelf: 'center',
+        width: '80%',
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)',
+    },
+    stats:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: 'red'
+    },
+    incomeContainer: {
+        flexDirection: "column",
+        padding: 5,
+    },
+    income: {
+        flexDirection: 'row',
+        gap: 10,
+    },
+    balance: {
+        color: 'black',
+        size: 30,
+        fontWeight: 'bold',
+    },
+})
