6d22d472e9f69806fcda21b08ad722482eac23d4
Jlara38 <chuplaxlara@gmail.com>
2026-05-26 02:52:39 -0500

    Text Input component has been made and is looking closer to the idea in mind. Modal now has 3 different ways to close it. * Default device back button, * A back button, and a part of the screen where you can just close out the modal without tapping the buttons,.

--- modals/TransactionModal.jsx
+++ modals/TransactionModal.jsx
@@ -1,21 +1,36 @@
 /* This will be the section that focuses on importing stuff from Expo, React, and React Native */
-import React from 'react'
-import {Modal, Text, View, useState, useEffect, TouchableOpacity, StyleSheet} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {Modal, Text, View, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, TextInput} from 'react-native'
+
+/* This will be the section that focuses on importing stuff from the already made components */
+import TxtInput from '../components/TxtInput'
+
+/* This will be the section that focuses on importing stuff that are images,videos, icons, etc. (Stuff that isnt components). */
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TransactionModal  ({modalVisible, setModalVisible}){
   return(
     <Modal
         visible={modalVisible}
         transparent={true}
-        animationType='slide'
+        animationType='fade'
         onRequestClose={() => setModalVisible(false)}>
         <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-            <Text> Add Expense </Text>
+            {/* The small greyed out area that allows the person to close the modal without pressing the close or back button. */}
+            <TouchableWithoutFeedback onPress={() => setModalVisible(false)} style={styles.closeArea}>
+                <View style={styles.closeArea}></View>
+            </TouchableWithoutFeedback>
+
+            {/* The actual container being used to display the modal content.  */}
+            <View style={styles.modalBox}>
+
             {/* Close Button */}
-            <TouchableOpacity onPress={() => setModalVisible(false)}>
-                <Text> Cancel </Text>
+            <TouchableOpacity style={styles.closeButton}>
+                <AntDesign name="close" size={24} color="black" />
             </TouchableOpacity>
+
+            <TxtInput></TxtInput>
+
             </View>
         </View>
     </Modal>
@@ -23,19 +38,34 @@ export default function TransactionModal  ({modalVisible, setModalVisible}){
 }
 
 const styles = StyleSheet.create({
+    // specifies how much space the modal container will use up in regards to the screen and how it will be positioned. 
     modalContainer: {
         flex: 1,
         justifyContent: 'flex-end',
         alignItems: 'center',
         backgroundColor: 'rgba(0,0,0,0.5)'
     },
-    
-    modalContent: {
+
+    // specifies the area the grey area to that can be used to close the modal will use.
+    closeArea: {
+        flex: 1,
         width: '100%',
-        height: '90%',
+    },
+    
+    // Defines the actual size of the modal box that the user will be able to interact with. 
+    // Effectively sharing part of the space that the modal container has with the close area. 
+    modalBox: {
+        width: '100%',
+        height: '85%',
         backgroundColor: '#fff',
         borderRadius: 10,
         padding: 20,
         gap: 10,
+    },
+
+    // Specifically made so that the button sticks to the end of the modal box so that the user 
+    // can close the modal with the button as well. 
+    closeButton: {
+        alignSelf: 'flex-end',
     },
 })
